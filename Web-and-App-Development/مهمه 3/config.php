1dd4e513087d179666c4e479f59dc44d68514b64
Dania-Alharbi <dmmmp0t@gmail.com>
2026-08-04 03:48:54 +0300

    Create config.php

---
+++ Web-and-App-Development/مهمه 3/config.php
@@ -0,0 +1,8 @@
+
+// config.php طريقة كتابة كود 
+
+<?ph
+
+$apiKey = 'YOUR_API_KEY_HERE';( بيكون داخله مفتاح API)
+                                                                                                            
+?>
